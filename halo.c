cfe796406df8399c0bb7d5b3e2118e2ee0372e57
adamzvx <adammmfs@gmail.com>
2019-12-24 09:15:16 +0700

    first

---
+++ halo.c
@@ -0,0 +1,13 @@
+#ifndef HALO_C_INCLUDED
+#define HALO_C_INCLUDED
+
+
+
+#endif // HALO_C_INCLUDED
+main(){
+
+printf("hlo");
+printf("hlo");
+printf("hlo");
+printf("hlo");printf("hlo");printf("hlo");printf("hlo");printf("hlo");
+}
---
+++ halo2.c
@@ -0,0 +1,14 @@
+#ifndef HALO_C_INCLUDED
+#define HALO_C_INCLUDED
+
+
+
+#endif // HALO_C_INCLUDED
+main(){
+
+printf("TESSSSSSSSSSS");
+printf("hlo");
+printf("hlo");
+printf("hlo");
+printf("hlo");printf("hlo");printf("hlo");printf("hlo");printf("hlo");
+}
